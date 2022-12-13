7b9555e8f3cbf204e6971ab0dbf91642dc437980
happydoggie666 <115023879+happydoggie666@users.noreply.github.com>
2022-12-13 15:11:43 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,11 +1,10 @@
-import Iterative from "./components/project_page/Iterative";
-
 export const projects = [
     {
       title: "Iterative Design",
       subtitle: "Grocery Delivery Apps",
-      description:
-        "My team and I designed a mobile APP interface for an emerging startup named Dropezy that delivers groceries in Indonesia. We sketched ideas for the interface and then created an interactive interface. To revise our design, we conducted user testing on our interactive interface.",
+      // description:
+      //   "My team and I designed a mobile APP interface for an emerging startup named Dropezy that delivers groceries in Indonesia. We sketched ideas for the interface and then created an interactive interface. To revise our design, we conducted user testing on our interactive interface.",
+      description:"My team and I designed a mobile APP interface for an emerging startup named Dropezy that intends to provide grocery delivery of groceries within 15 mins service in Indonesia. We first sketched ideas for the interface and made high-fidelity prototypes based on our sketching. Then we did user testing on our interactive design. Afterwards, we redesigned our interface based on users’ feedback.",
       image: "image/dropezy.png",
       // link:"<Iterative/>",
       link: "https://happydoggie666.github.io/Iterative-Design/",
@@ -14,7 +13,7 @@ export const projects = [
       title: "Responsive Redesign",
       subtitle: "Ling's Car Website Redesign",
       description:
-        "A responsive website could handle different screen sizes: mobile, tablet, and desktop. I analyzed and identified flaws in an existing LingsCars website, constructed low-fidelity and high-fidelity prototypes for various screen sizes, and finally built this responsive website based on those prototypes.",
+        "A responsive website could handle different screen sizes: mobile, tablet, and desktop. I analyzed and identified flaws in an existing website (LingsCars.com), constructed low-fidelity and high-fidelity prototypes for various screen sizes, and finally built an responsive website based on those prototypes.",
       image: "image/responsive.jpg",
       link: "https://happydoggie666.github.io/Redesign/",
     },
@@ -22,19 +21,18 @@ export const projects = [
       title: "Persona",
       subtitle: "A Case Study about Coffee Machine Usage",
       description:
-        "In this case study, I observed and interviewed real users about their experience interacting with coffee machine at CIT 4th floor. Stepped into a user’s shoes, I created personas based on these users and illustrated a storyboard for one of my interviewees.",
+        "In this case study, I observed and interviewed real users about their experience interacting with the coffee machine at CIT 4th floor. Stepped into a user’s shoes, I created personas based on these users and illustrated a storyboard for one of my interviewees.",
       image: "image/Persona.jpg",
       link: "https://happydoggie666.github.io/Persona-Storyboarding-1300/",
     },
     {
-      title: "Shopping Website",
-      subtitle: "A React Shopping Website",
+      title: "Bakery Store Website",
+      subtitle: "A Simple React Bakery Store Website",
       description:
-        "Online shopping through app and website has become very popular nowadays. Here, I designed a bakery shopping shop that is intuitive and easy to use. Users can easily use the filtering buttons and sorting fields to find their desired items. The total price is calculated and interface is transparent.",
+        "Online shopping through apps and websites has become very popular nowadays. Here, I designed a bakery shopping shop that is intuitive and easy to use. Users can easily use the filtering buttons and sorting fields to find their desired items. The total price is calculated and interface is transparent.",
       image: "image/bakery_dev.webp",
       link: "https://happydoggie666.github.io/bakery_dev/",
+      // link: <Shopping/>
     },
 
   ];
-
-  export const skills = ["UI", "frontend"];
