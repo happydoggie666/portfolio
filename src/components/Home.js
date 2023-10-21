004ae152f1d6bc91cf7c3383c0b3212a87d4b4a5
happydoggie666 <115023879+happydoggie666@users.noreply.github.com>
2023-10-21 16:16:21 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,44 +1,45 @@
 import React from "react";
-import {Typography, Avatar, Grid, Box} from "@material-ui/core";
+import { Typography, Avatar, Grid, Box } from "@material-ui/core";
 import Typed from "react-typed";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 // import Particles from "react-particles-js";
 
-
 // With the help from: https://www.youtube.com/watch?v=9Q555VoPe4M   50min
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   avatar: {
     width: theme.spacing(15),
     height: theme.spacing(15),
     margin: theme.spacing(1),
   },
-  title:{
-    color:"#FF5833"
+  title: {
+    color: "#FF5833",
   },
-  subtitle:{
-    color:"#096213",
-    marginBottom:"3rem"
+  subtitle: {
+    color: "#096213",
+    marginBottom: "3rem",
   },
-  container:{
+  container: {
     position: "absolute",
     top: "45%",
-    left:"50%",
-    transform:"translate(-50%, -50%)",
-    width:"100vw",
-    textAlign:"center",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "100vw",
+    textAlign: "center",
     zIndex: 1,
-    
-  }
-}))
+  },
+}));
 
-export default function Home() { 
+export default function Home() {
   const classes = useStyles();
 
   return (
-    <section id="home" className="homebg" >
+    <section id="home" className="homebg">
       {/* style={{backgroundImage: `url("image/head_bg.jfif")`, backgroundRepeat: "no-repeat"}}> */}
       {/* <div style={{backgroundImage: `url("image/head_bg.jfif")`}}> */}
-      <Box className={classes.container} style={{backgroundColor:'aliceblue', width:"35%"}}>
+      <Box
+        className={classes.container}
+        style={{ backgroundColor: "aliceblue", width: "35%" }}
+      >
         {/* <Particles 
           params ={{
             particles:{
@@ -49,18 +50,28 @@ export default function Home() {
         }}
         /> */}
         <Grid container justify="center">
-          <Avatar className={classes.avatar} src={'image/happy_doggy.jfif'} alt="HappyDoggy666" />
+          <Avatar
+            className={classes.avatar}
+            src={"image/chen wei.jpg"}
+            alt="Chen Wei"
+          />
         </Grid>
-        <Typography className={classes.title} variant="h4"> 
-          <Typed strings={["Happy Doggie666"]} typeSpeed={40}/>
+        <Typography className={classes.title} variant="h4">
+          <Typed strings={["Chen Wei"]} typeSpeed={40} />
         </Typography>
-        <br/>
-        <Typography variant="h5" className={classes.subtitle}> 
-          <Typed 
-          strings={["Web Development", "Machine Learning","UIUX Design"]} 
-          typeSpeed={40}
-          backSpeed={60}
-          loop
+        <br />
+        <Typography variant="h5" className={classes.subtitle}>
+          <Typed
+            strings={[
+              "Web Developer",
+              "Machine Learning",
+              "UIUX Design",
+              "Data Scientist",
+              "Data Analyst",
+            ]}
+            typeSpeed={40}
+            backSpeed={60}
+            loop
           />
         </Typography>
       </Box>
