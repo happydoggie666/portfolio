0015e41ae7d96e83e32d84512d7903ce720cc4ed
happydoggie666 <115023879+happydoggie666@users.noreply.github.com>
2023-10-21 16:10:51 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 
-
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 export default function Footer() {
   const needToTop = () => {
@@ -17,53 +16,80 @@ export default function Footer() {
     }
   };
   useEffect(() => {
-    
     window.addEventListener("scroll", needToTop);
     return () => window.removeEventListener("scroll", needToTop);
-  })
+  });
   // useEffect(() => {
   //     window.addEventListener("scroll", onScroll);
   //     return () => window.removeEventListener("scroll", onScroll);
   // })
-    return (
-    <section id="contact" className="relative"  style={{marginTop: "5rem"}}>
-        <footer className="footerSty text-white">
-            <div className="footer-top d-flex justify-content-center">
-                <div className="container">
-                  <div className="col">
-                    <div className="row">
-                      <div className="col-lg-6 col-sm-12 px-5 py-3">
-                        <h4><a href="#">Happy Doggie 666</a></h4>
-                        <p>Stay Hungry, Stay Foolish!</p>
-                        <ul className="navbar-nav flex-row">
-                          <li className="nav-item mx-1">
-                              <a className="fa fa-facebook icon" href="#"/>
-                          </li>
-                          <li className="nav-item mx-1">
-                          <a className="fa fa-twitter icon" href="#"/>
-                          </li>
-                          <li className="nav-item mx-1">
-                              <a className="fa fa-github icon" href="#"><i className="ri-instagram-line"></i></a>
-                          </li>
-                          <li className="nav-item mx-1">
-                              <a className="fa fa-linkedin-square icon" href="#"><i className="ri-youtube-line"></i></a>
-                          </li>
-                        </ul>
-
-                        
-                      </div>
-
-                      <div className="col-lg-6 col-sm-12 px-5 py-3 center">
-                        <a href="#" className="topBtn" id="topBtn" style={{marginRight:"10%"}}>Back to Top &#8593;</a>
-                        {/* Craft with 💘 */}
-
-                        Craft with <font color="hotpink" style={{marginLeft:"5px"}}>♥</font>
-                      </div>
-                    </div>
-                  </div>
+  return (
+    <section id="contact" className="relative" style={{ marginTop: "5rem" }}>
+      <footer className="footerSty text-white">
+        <div className="footer-top d-flex justify-content-center">
+          <div className="container">
+            <div className="col">
+              <div className="row">
+                <div className="col-lg-6 col-sm-12 px-5 py-3">
+                  <h4>
+                    <a href="#">Happy Doggie 666</a>
+                  </h4>
+                  <p>Stay Hungry, Stay Foolish!</p>
+                  <ul className="navbar-nav flex-row">
+                    <li className="nav-item mx-1">
+                      <a
+                        className="fa fa-facebook icon"
+                        href="https://www.facebook.com/profile.php?id=100004302346962"
+                      />
+                    </li>
+                    {/* <li className="nav-item mx-1">
+                      <a className="fa fa-twitter icon" href="#" />
+                    </li> */}
+                    <li className="nav-item mx-1">
+                      <a
+                        className="fa fa-instagram icon"
+                        href="https://www.instagram.com/mrsa_8/?hl=en"
+                      />
+                    </li>
+                    <li className="nav-item mx-1">
+                      <a
+                        className="fa fa-github icon"
+                        href="https://github.com/MRSA-J"
+                      >
+                        <i className="ri-instagram-line"></i>
+                      </a>
+                    </li>
+                    <li className="nav-item mx-1">
+                      <a
+                        className="fa fa-linkedin-square icon"
+                        href="https://www.linkedin.com/in/chen-wei-57b225198/#"
+                      >
+                        <i className="ri-youtube-line"></i>
+                      </a>
+                    </li>
+                  </ul>
                 </div>
+
+                <div className="col-lg-6 col-sm-12 px-5 py-3 center">
+                  <a
+                    href="#"
+                    className="topBtn"
+                    id="topBtn"
+                    style={{ marginRight: "10%" }}
+                  >
+                    Back to Top &#8593;
+                  </a>
+                  {/* Craft with 💘 */}
+                  Craft with{" "}
+                  <font color="hotpink" style={{ marginLeft: "5px" }}>
+                    ♥
+                  </font>
+                </div>
+              </div>
             </div>
-        </footer>
+          </div>
+        </div>
+      </footer>
     </section>
   );
 }
