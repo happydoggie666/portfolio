9fc4f3fd850b26fc71570a2835bb86245154d59c
happydoggie666 <115023879+happydoggie666@users.noreply.github.com>
2023-10-21 16:17:14 -0400

    Update TopNav.js

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react';
-import {Box} from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
-import { isBrowser } from 'react-device-detect';
+import React, { useEffect } from "react";
+import { Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { isBrowser } from "react-device-detect";
 
 const Navbar = () => {
-  const useStyles = makeStyles(theme => ({
-    container:{
+  const useStyles = makeStyles((theme) => ({
+    container: {
       position: "relative",
-    }
-  }))
+    },
+  }));
   function jumpToTarget(idName) {
     var element = document.getElementById(idName);
     var positionX = element.offsetLeft;
-    var positionY = element.offsetTop-100;
-    window.scrollTo(positionX, positionY); 
+    var positionY = element.offsetTop - 100;
+    window.scrollTo(positionX, positionY);
   }
   const onScroll = () => {
     let pixelsFromTop = window.scrollY;
@@ -28,19 +28,35 @@ const Navbar = () => {
       window.addEventListener("scroll", onScroll);
       return () => window.removeEventListener("scroll", onScroll);
     }
-  })
-  
-  
+  });
 
   const classes = useStyles();
 
   return (
     <Box className={classes.container}>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{width: "100%"}}>
+      <nav
+        className="navbar navbar-expand-lg navbar-light bg-light"
+        style={{ width: "100%" }}
+      >
         <div>
-          <p className="navbar-brand" style={{fontFamily: "sans-serif", fontWeight: "400", fontSize: "1.3rem", cursor: "pointer"}} onClick={() => jumpToTarget('home')}>
-            <img src={'image/happy_doggy.jfif'} weight={100} height={45} alt="logo" style={{margin: "0 5px"}}/>
-            [HappyDoggie666]
+          <p
+            className="navbar-brand"
+            style={{
+              fontFamily: "sans-serif",
+              fontWeight: "400",
+              fontSize: "1.3rem",
+              cursor: "pointer",
+            }}
+            onClick={() => jumpToTarget("home")}
+          >
+            <img
+              src={"image/happy_doggy.jfif"}
+              weight={100}
+              height={45}
+              alt="logo"
+              style={{ margin: "0 5px" }}
+            />
+            Chen Wei
           </p>
         </div>
         <button
@@ -54,25 +70,37 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse  mr-3 justify-content-end" id="navbarNavDropdown">
+        <div
+          className="collapse navbar-collapse  mr-3 justify-content-end"
+          id="navbarNavDropdown"
+        >
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="navbar-button" onClick={() => jumpToTarget('home')}>
+              <a className="navbar-button" onClick={() => jumpToTarget("home")}>
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="navbar-button"  onClick={() => jumpToTarget('about')}>
+              <a
+                className="navbar-button"
+                onClick={() => jumpToTarget("about")}
+              >
                 About
               </a>
             </li>
             <li className="nav-item">
-              <a className="navbar-button" onClick={() => jumpToTarget('projects')}>
-                  Projects
+              <a
+                className="navbar-button"
+                onClick={() => jumpToTarget("projects")}
+              >
+                Projects
               </a>
             </li>
             <li className="nav-item dropdown">
-              <a className="navbar-button" onClick={() => jumpToTarget('contact')}>
+              <a
+                className="navbar-button"
+                onClick={() => jumpToTarget("contact")}
+              >
                 Contact
               </a>
               {/* <div
@@ -93,9 +121,8 @@ const Navbar = () => {
           </ul>
         </div>
       </nav>
-      <div className="progress" id="progress"/>;
+      <div className="progress" id="progress" />;
     </Box>
-
   );
 };
 
